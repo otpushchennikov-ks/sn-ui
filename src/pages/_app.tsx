123e2a66a485f30e15efde6d09a3bea05c212682
Konstantin <otpushchennikov.ks@gmail.com>
2021-05-01 23:16:00 +0300

    _app: use github-token in apollo's headers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,26 @@
 import 'antd/dist/antd.css';
 import { AppProps } from 'next/app';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { ApolloProvider } from '@apollo/client/react';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
+
+const httpLink = createHttpLink({ uri: 'http://localhost:4000/graphql' });
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('github-token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ?? '',
+    },
+  };
+});
+const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const GlobalStyle = createGlobalStyle`
   * { box-sizing: border-box; }
@@ -20,11 +39,11 @@ const theme = {
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ApolloProvider client={apolloClient}>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </ApolloProvider>
   );
 }
